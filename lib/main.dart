27cbc226e89d39bee3466ecedb92e7e9eb569d51
LiteTekno92 <lite.tekno92@gmail.com>
2019-03-30 10:46:47 +0100

    Main changed

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+// main.dart
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+@override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Scaffold Widget Tutorial',
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Flutter Scaffold Example'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.zero,
+        child: new Scaffold(
+          backgroundColor: Colors.amber,
+        ));
+  }
+}
